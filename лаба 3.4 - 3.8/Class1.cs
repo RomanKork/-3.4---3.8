6d1f74005125e3b2be4ff2fb9f39305fd57a7780
User <User@WIN-CG1M31PQOK0>
2024-11-13 13:26:23 +0500

    Добавьте файлы проекта.

---
+++ лаба 3.4 - 3.8/Class1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace лаба_3._4___3._8
+{
+    public static class FileTasks
+    {
+        // Задание 4: Заполнить бинарный файл случайными числами и найти наибольший модуль элемента с нечетным индексом
+        public static void GenerateBinaryFileWithNumbers(string filePath, int count)
+        {
+            var random = new Random();
+            using (var writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    writer.Write(random.Next(-100, 100));
+                }
+            }
+        }
+
+        public static int FindMaxOddIndexAbsoluteValue(string filePath)
+        {
+            int maxAbsValue = 0;
+            using (var reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+            {
+                int index = 1;
+                while (reader.BaseStream.Position != reader.BaseStream.Length)
+                {
+                    int value = reader.ReadInt32();
+                    if (index % 2 != 0)
+                    {
+                        maxAbsValue = Math.Max(maxAbsValue, Math.Abs(value));
+                    }
+                    index++;
+                }
+            }
+            return maxAbsValue;
+        }
+
+        // Задание 5: Структура "Игрушка" и бинарный файл с XML-сериализацией
+        public struct Toy
+        {
+            public string Name;
+            public int Price;
+            public int MinAge;
+            public int MaxAge;
+        }
+
+        public static void GenerateBinaryFileWithToys(string filePath, List<Toy> toys)
+        {
+            var serializer = new XmlSerializer(typeof(List<Toy>));
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                serializer.Serialize(stream, toys);
+            }
+        }
+
+        public static List<string> GetToysForAgeRange(string filePath, int minAge, int maxAge)
+        {
+            var result = new List<string>();
+            var serializer = new XmlSerializer(typeof(List<Toy>));
+
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                var toys = (List<Toy>)serializer.Deserialize(stream);
+                result.AddRange(toys.Where(toy => toy.MinAge <= maxAge && toy.MaxAge >= minAge)
+                                    .Select(toy => toy.Name));
+            }
+            return result;
+        }
+
+        // Задание 6: Найти среднее арифметическое чисел в текстовом файле
+        public static void GenerateTextFileWithNumbers(string filePath, int count)
+        {
+            var random = new Random();
+            using (var writer = new StreamWriter(filePath))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    writer.WriteLine(random.Next(-100, 100));
+                }
+            }
+        }
+
+        public static double CalculateAverage(string filePath)
+        {
+            var numbers = File.ReadAllLines(filePath).Select(int.Parse);
+            return numbers.Average();
+        }
+
+        // Задание 7: Найти произведение нечетных чисел в текстовом файле
+        public static void GenerateTextFileWithMultipleNumbersPerLine(string filePath, int lines, int numbersPerLine)
+        {
+            var random = new Random();
+            using (var writer = new StreamWriter(filePath))
+            {
+                for (int i = 0; i < lines; i++)
+                {
+                    var numbers = new int[numbersPerLine];
+                    for (int j = 0; j < numbersPerLine; j++)
+                    {
+                        numbers[j] = random.Next(-20, 20);
+                    }
+                    writer.WriteLine(string.Join(" ", numbers));
+                }
+            }
+        }
+
+        public static long CalculateOddProduct(string filePath)
+        {
+            long product = 1;
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                var numbers = line.Split(' ').Select(int.Parse);
+                foreach (var number in numbers)
+                {
+                    if (number % 2 != 0)
+                    {
+                        product *= number;
+                    }
+                }
+            }
+            return product;
+        }
+
+        // Задание 8: Переписать строки без букв в другой файл
+        public static void CopyLinesWithoutLetters(string inputFilePath, string outputFilePath)
+        {
+            using (var reader = new StreamReader(inputFilePath))
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!line.Any(char.IsLetter))
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ лаба 3.4 - 3.8/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+namespace лаба_3._4___3._8
+{
+    internal class Programm
+    {
+        private static void Main(string[] args)
+        {
+            // Задание 4
+            FileTasks.GenerateBinaryFileWithNumbers("numbers.bin", 100);
+            int maxAbsOdd = FileTasks.FindMaxOddIndexAbsoluteValue("numbers.bin");
+
+            // Задание 5
+            var toys = new List<FileTasks.Toy>
+{
+    new FileTasks.Toy { Name = "Bear", Price = 500, MinAge = 2, MaxAge = 5 },
+    new FileTasks.Toy { Name = "Car", Price = 300, MinAge = 4, MaxAge = 6 },
+};
+            FileTasks.GenerateBinaryFileWithToys("C:\\Users\\User\\Desktop\\toys.xml", toys);
+            var toysForFourToFiveYears = FileTasks.GetToysForAgeRange("C:\\Users\\User\\Desktop\\toys.xml", 4, 5);
+
+            // Задание 6
+            Console.WriteLine("Введите количество чисел в файле, они сгенерируются автоматический: ");
+           // FileTasks.GenerateTextFileWithNumbers("C:\\Users\\User\\Desktop\\numbers.txt", Check());
+            double average = FileTasks.CalculateAverage("C:\\Users\\User\\Desktop\\numbers.txt");
+            //Console.WriteLine("Среднее арифметическое чисел: " + average);
+
+            // Задание 7
+            Console.WriteLine("Введите количество строк и количество чисел в строке: ");
+            FileTasks.GenerateTextFileWithMultipleNumbersPerLine("C:\\Users\\User\\Desktop\\multiple_numbers.txt", Check(), Check());
+            long oddProduct = FileTasks.CalculateOddProduct("C:\\Users\\User\\Desktop\\multiple_numbers.txt");
+            Console.WriteLine("Произведение всех нечётных чисел: " + oddProduct);
+
+            // Задание 8
+            FileTasks.CopyLinesWithoutLetters("C:\\Users\\User\\Desktop\\textfile.txt", "C:\\Users\\User\\Desktop\\outputfile.txt");
+
+        }
+        public static int Check()
+        {
+            int a;
+            if (int.TryParse(Console.ReadLine(), out a) && a > 0)
+            {
+                return a;
+            }
+            Console.WriteLine("Вы ввели некорректное значение, введите значение вновь: ");
+            return Check();
+        }
+    }
+}
