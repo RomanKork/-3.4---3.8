98f4fe23e6ac8994a2a65876af1c697cb90157dd
RomanKork <144647881+RomanKork@users.noreply.github.com>
2024-11-13 14:13:07 +0500

    Update Program.cs

--- лаба 3.4 - 3.8/Program.cs
+++ лаба 3.4 - 3.8/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,33 +9,24 @@ namespace лаба_3._4___3._8
     {
         private static void Main(string[] args)
         {
-            // Задание 4
-            FileTasks.GenerateBinaryFileWithNumbers("numbers.bin", 100);
-            int maxAbsOdd = FileTasks.FindMaxOddIndexAbsoluteValue("numbers.bin");
-
-            // Задание 5
-            var toys = new List<FileTasks.Toy>
-{
-    new FileTasks.Toy { Name = "Bear", Price = 500, MinAge = 2, MaxAge = 5 },
-    new FileTasks.Toy { Name = "Car", Price = 300, MinAge = 4, MaxAge = 6 },
-};
-            FileTasks.GenerateBinaryFileWithToys("C:\\Users\\User\\Desktop\\toys.xml", toys);
-            var toysForFourToFiveYears = FileTasks.GetToysForAgeRange("C:\\Users\\User\\Desktop\\toys.xml", 4, 5);
-
-            // Задание 6
             Console.WriteLine("Введите количество чисел в файле, они сгенерируются автоматический: ");
-           // FileTasks.GenerateTextFileWithNumbers("C:\\Users\\User\\Desktop\\numbers.txt", Check());
-            double average = FileTasks.CalculateAverage("C:\\Users\\User\\Desktop\\numbers.txt");
-            //Console.WriteLine("Среднее арифметическое чисел: " + average);
+            FileTasks.GenerateBinaryFileWithRandomNumbers("numbers.bin", Check());
+            int maxAbsOdd = FileTasks.FindMaxOddIndexAbsoluteValue("numbers.bin");
+            Console.WriteLine("Максимальный модуль числа: " + maxAbsOdd);
 
-            // Задание 7
+            Console.WriteLine("Введите количество чисел в файле, они сгенерируются автоматический: ");
+            FileTasks.GenerateTextFileWithNumbers("C:\\Users\\User\\Desktop\\numbers.txt", Check());
+            double average = FileTasks.CalculateAverage("C:\\Users\\User\\Desktop\\numbers.txt");
+            Console.WriteLine("Среднее арифметическое чисел: " + average);
+
             Console.WriteLine("Введите количество строк и количество чисел в строке: ");
             FileTasks.GenerateTextFileWithMultipleNumbersPerLine("C:\\Users\\User\\Desktop\\multiple_numbers.txt", Check(), Check());
             long oddProduct = FileTasks.CalculateOddProduct("C:\\Users\\User\\Desktop\\multiple_numbers.txt");
             Console.WriteLine("Произведение всех нечётных чисел: " + oddProduct);
 
-            // Задание 8
             FileTasks.CopyLinesWithoutLetters("C:\\Users\\User\\Desktop\\textfile.txt", "C:\\Users\\User\\Desktop\\outputfile.txt");
+            Console.WriteLine("Новый файл создан.");
+
 
         }
         public static int Check()
